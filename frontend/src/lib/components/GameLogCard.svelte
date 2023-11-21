e5993fd3b9e347789ab1e711667ec7741b20dfae
Isaiah <isaiahmstockton@gmail.com>
2023-11-20 20:15:59 -0500

    add svelte typescript support

---
+++ frontend/src/lib/components/GameLogCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { main } from "../wailsjs/go/models";
+
+    export let gameLog: main.Log;
+</script>
+
+<div class="card bg-base-100 shadow-xl">
+    <div class="card-body">
+        <p class="card-title">{gameLog.title}</p>
+        <p class="card-subtitle">{gameLog.notes}</p>
+    </div>
+</div>
