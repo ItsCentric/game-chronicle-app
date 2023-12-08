c75d791d915cba99ef56b47ccfb0f11b61b06638
Isaiah <isaiahmstockton@gmail.com>
2023-12-07 22:32:07 -0500

    add rating reset

--- frontend/src/lib/components/RatingInput.svelte
+++ frontend/src/lib/components/RatingInput.svelte
@@ -2,62 +2,68 @@
 	<input
 		type="radio"
 		name="rating"
-		value="0.5"
+		value={0}
+		class="rating-hidden border-0 checked:!bg-transparent checked:!ring-transparent"
+        checked
+	/>
+	<input
+		type="radio"
+		name="rating"
+		value={0.5}
 		class="mask mask-star-2 mask-half-1 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="1"
+		value={1}
 		class="mask mask-star-2 mask-half-2 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="1.5"
+		value={1.5}
 		class="mask mask-star-2 mask-half-1 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="2"
+		value={2}
 		class="mask mask-star-2 mask-half-2 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="2.5"
+		value={2.5}
 		class="mask mask-star-2 mask-half-1 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="3"
+		value={3}
 		class="mask mask-star-2 mask-half-2 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="3.5"
+		value={3.5}
 		class="mask mask-star-2 mask-half-1 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="4"
+		value={4}
 		class="mask mask-star-2 mask-half-2 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="4.5"
+		value={4.5}
 		class="mask mask-star-2 mask-half-1 bg-yellow-500 border-0 checked:!bg-yellow-500"
 	/>
 	<input
 		type="radio"
 		name="rating"
-		value="5"
+		value={5}
 		class="mask mask-star-2 mask-half-2 bg-yellow-500 border-0 checked:!bg-yellow-500"
-		checked
 	/>
 </div>
