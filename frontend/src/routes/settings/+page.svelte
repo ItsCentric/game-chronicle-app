051c00d9c8f0246644a2d3431e128a6b73d7cfbd
Isaiah <isaiahmstockton@gmail.com>
2024-03-13 16:32:36 -0400

    convert settings page

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,28 +1,44 @@
 <script lang="ts">
 	import { settingsSchema } from '$lib/schemas';
 	import { OpenDirectoryDialog } from '$lib/wailsjs/go/main/App';
-	import { SaveUserSettings, GetUserSettings } from '$lib/wailsjs/go/main/Database';
+	import { GetUserSettings, SaveUserSettings } from '$lib/wailsjs/go/main/Database';
 	import { WindowReloadApp } from '$lib/wailsjs/runtime/runtime';
-	import { validator } from '@felte/validator-zod';
-	import { createForm } from 'felte';
 	import { ArrowLeft, PencilIcon, Trash } from 'lucide-svelte';
+	import { defaults, superForm } from 'sveltekit-superforms';
+	import * as Form from '$lib/components/ui/form';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import * as Table from '$lib/components/ui/table';
+	import { Switch } from '$lib/components/ui/switch';
+	import { Button } from '$lib/components/ui/button';
+	import { zod } from 'sveltekit-superforms/adapters';
 	import { onMount } from 'svelte';
-	import type { z } from 'zod';
 
-	var reloadApplicationModal: HTMLDialogElement | null;
-	const { form, isValid, isDirty, setData, data, setInitialValues, setIsDirty } = createForm<
-		z.infer<typeof settingsSchema>
-	>({
-		initialValues: { processMonitoringEnabled: false, executablePaths: [] },
-		onSubmit: async (values) => {
-			var newSettings = {
-				processMonitoringEnabled: values.processMonitoringEnabled,
-				executablePaths: values.executablePaths.join(';')
-			};
-			await SaveUserSettings(newSettings);
-			reloadApplicationModal?.showModal();
-		},
-		extend: validator({ schema: settingsSchema })
+	let openReloadApplicationModal = false;
+	const settingsForm = superForm(defaults(zod(settingsSchema)), {
+		validators: zod(settingsSchema),
+		SPA: true,
+		onUpdate: async ({ form }) => {
+			if (form.valid) {
+				var newSettings = {
+					processMonitoringEnabled: form.data.processMonitoringEnabled,
+					executablePaths: form.data.executablePaths.join(';')
+				};
+				await SaveUserSettings(newSettings);
+				openReloadApplicationModal = true;
+			}
+		}
+	});
+	const { form: settingsFormData, enhance: settingsFormEnhance } = settingsForm;
+
+	onMount(async () => {
+		const userPreferencesRes = await GetUserSettings();
+		if (!userPreferencesRes.preferences) return;
+		const executablePathsString = userPreferencesRes.preferences.executablePaths;
+		let executablePathsArray = executablePathsString.split(';');
+		executablePathsArray = executablePathsArray.filter((path) => path !== '');
+		$settingsFormData.processMonitoringEnabled =
+			userPreferencesRes.preferences.processMonitoringEnabled;
+		$settingsFormData.executablePaths = executablePathsArray;
 	});
 
 	async function newDirectoryDialog() {
@@ -38,95 +54,82 @@
 			addPath(result.selectedDirectory);
 		}
 	}
-	onMount(async () => {
-		var userPreferencesRes = await GetUserSettings();
-		if (userPreferencesRes.preferences) {
-			var executablePathsString = userPreferencesRes.preferences.executablePaths;
-			var executablePathsArray = executablePathsString.split(';');
-			executablePathsArray = executablePathsArray.filter((path) => path !== '');
-			setInitialValues({
-				processMonitoringEnabled: userPreferencesRes.preferences.processMonitoringEnabled,
-				executablePaths: executablePathsArray
-			});
-			setData('executablePaths', executablePathsArray);
-			setData('processMonitoringEnabled', userPreferencesRes.preferences.processMonitoringEnabled);
-		}
-	});
 	function removePath(path: string) {
-		var newExecutablePathsArray = $data.executablePaths.filter((p) => p !== path);
-		setData('executablePaths', newExecutablePathsArray);
-		setIsDirty(true);
+		$settingsFormData.executablePaths = $settingsFormData.executablePaths.filter((p) => p !== path);
 	}
 	function addPath(path: string) {
-		setData('executablePaths', [...$data.executablePaths, path]);
-		setIsDirty(true);
+		$settingsFormData.executablePaths = [...$settingsFormData.executablePaths, path];
 	}
 </script>
 
 <main class="w-full h-full p-12 flex-col justify-center items-center">
 	<div class="flex gap-2 mb-8 items-center">
-		<a href="/" class="btn btn-circle btn-ghost"><ArrowLeft size={32} /></a>
+		<Button href="/" variant="ghost"><ArrowLeft size={32} /></Button>
 		<h1 class="text-3xl font-bold">Settings</h1>
 	</div>
-	<form use:form class="flex flex-col gap-8">
+	<form method="post" use:settingsFormEnhance class="flex flex-col gap-8">
 		<section class="flex flex-col gap-2">
 			<div class="flex justify-between mb-2">
 				<h2 class="text-2xl font-bold">Monitoring</h2>
 			</div>
-			<div class="form-control">
-				<label class="label cursor-pointer">
-					<span class="label-text">Enable Game Monitoring</span>
-					<input
-						type="checkbox"
-						class="toggle"
-						name="processMonitoringEnabled"
-						checked={$data.processMonitoringEnabled}
-					/>
-				</label>
-			</div>
+			<Form.Field form={settingsForm} name="processMonitoringEnabled">
+				<Form.Control let:attrs>
+					<div class="flex justify-between items-center">
+						<Form.Label>Enable Game Monitoring</Form.Label>
+						<Switch
+							includeInput
+							{...attrs}
+							bind:checked={$settingsFormData.processMonitoringEnabled}
+						/>
+					</div>
+				</Form.Control>
+			</Form.Field>
 			<div class="flex justify-between items-center mb-2">
 				<h3 class="text-xl font-bold">Monitoring Paths</h3>
-				<button type="button" class="btn" on:click={newDirectoryDialog}>Add New Path</button>
+				<Button type="button" on:click={newDirectoryDialog}>Add New Path</Button>
 			</div>
-			{#if $data.executablePaths.length !== 0}
-				<div class="w-full border border-b-0 flex flex-col items-center rounded-md">
-					{#each $data.executablePaths as path}
-						<div class="w-full border-b flex items-center">
-							<p class="flex-1 p-4">{path}</p>
-							<div class="flex gap-2 p-4">
-								<button
-									type="button"
-									class="btn"
-									on:click={async () => await editDirectoryDialog(path)}
-									><PencilIcon height={12} width={12} /></button
-								>
-								<button type="button" class="btn" on:click={() => removePath(path)}
-									><Trash width={12} height={12} /></button
-								>
-							</div>
-						</div>
-					{/each}
-				</div>
+			{#if $settingsFormData.executablePaths.length !== 0}
+				<Table.Root>
+					<Table.Caption>Edit the system paths that should be monitored here.</Table.Caption>
+					<Table.Header>
+						<Table.Row>
+							<Table.Head>Path</Table.Head>
+							<Table.Head class="text-right">Actions</Table.Head>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+						{#each $settingsFormData.executablePaths as path}
+							<Table.Row>
+								<Table.Cell class="w-3/4">{path}</Table.Cell>
+								<Table.Cell class="text-right">
+									<Button type="button" on:click={async () => await editDirectoryDialog(path)}
+										><PencilIcon size={12} /></Button
+									>
+									<Button type="button" on:click={() => removePath(path)}
+										><Trash size={12} /></Button
+									>
+								</Table.Cell>
+							</Table.Row>
+						{/each}
+					</Table.Body>
+				</Table.Root>
 			{/if}
 		</section>
-		<dialog bind:this={reloadApplicationModal} class="modal">
-			<div class="modal-box">
-				<h3 class="font-bold text-lg">Take a Refresher</h3>
-				<p class="py-4">
+		<Button type="submit">Apply changes</Button>
+	</form>
+	<Dialog.Root bind:open={openReloadApplicationModal}>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Take a Refresher</Dialog.Title>
+				<Dialog.Description>
 					Looks like you changed some settings that require a reload to go into effect, please
 					reload at your earliest convenience.
-				</p>
-				<div class="modal-action">
-					<button class="btn" on:click={() => WindowReloadApp()}>Reload now</button>
-					<form method="dialog">
-						<button class="btn">I'll wait</button>
-					</form>
-				</div>
-			</div>
-			<form method="dialog" class="modal-backdrop">
-				<button class="cursor-default">close</button>
-			</form>
-		</dialog>
-		<button type="submit" class="btn" disabled={!$isValid || !$isDirty}>Apply changes</button>
-	</form>
+				</Dialog.Description>
+			</Dialog.Header>
+			<Dialog.Footer>
+				<Button on:click={() => WindowReloadApp()}>Reload now</Button>
+				<Button on:click={() => (openReloadApplicationModal = false)}>I'll wait</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
 </main>
