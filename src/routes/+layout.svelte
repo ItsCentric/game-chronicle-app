7a6fcb25c6a7a1639100524268cdb30614e99b70
Isaiah <isaiahmstockton@gmail.com>
2024-05-03 01:55:15 -0400

    add loading indicator for long navigations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,14 @@
 	import { goto } from '$app/navigation';
 	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query';
 	import { listen } from '@tauri-apps/api/event';
+	import { navigating } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
 	const queryClient = new QueryClient();
+	const loadProgress = tweened(0, { duration: 2500, easing: cubicOut });
+	let showProgress = false;
 
 	listen('game-stopped', (event) => {
 		const data = event.payload;
@@ -20,9 +26,29 @@
 			goto(`/logs/edit?gameId=${data.gameId}&minutesPlayed=${data.minutesPlayed}`);
 		}
 	});
+
+	$: if ($navigating) {
+		loadProgress.set(0.7, { duration: 2500 });
+		showProgress = true;
+		$navigating.complete.then(() => {
+			loadProgress.set(1, { duration: 400 }).then(() => {
+				showProgress = false;
+				loadProgress.set(0, { duration: 0 });
+			});
+		});
+	}
 </script>
 
-<main class="h-full">
+<main class="h-full relative">
+	{#if showProgress}
+		<div
+			in:fade={{ duration: 0, delay: 500 }}
+			out:fade={{ duration: 300 }}
+			class="absolute top-0 z-50 w-full"
+		>
+			<span class="bg-accent float-left h-1" style={`width: ${$loadProgress * 100}%`} />
+		</div>
+	{/if}
 	<Toaster />
 	<QueryClientProvider client={queryClient}>
 		<slot />
