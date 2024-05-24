d759b73601dffe51975a22b26f90c160ec2bd914
Isaiah <isaiahmstockton@gmail.com>
2024-05-24 15:25:16 -0400

    feat: cache random games response

--- src/routes/game-search/+page.ts
+++ src/routes/game-search/+page.ts
@@ -1,10 +1,21 @@
-import { authenticateWithTwitch, getRandomTopGames } from '$lib/rust-bindings/igdb';
+import { authenticateWithTwitch, getRandomTopGames, type IgdbGame } from '$lib/rust-bindings/igdb';
+import { writable } from 'svelte/store';
+
+const cachedResponse = writable<IgdbGame[] | null>(null);
 
 export const load = async () => {
 	if (typeof window === 'undefined') {
 		return { randomGames: [] };
 	}
+	let response: IgdbGame[] | null = null;
+	const unsubscribe = cachedResponse.subscribe((value) => (response = value));
+	if (response) {
+		unsubscribe();
+		return { randomGames: response };
+	}
 	const authenticateRes = await authenticateWithTwitch();
 	const randomGames = await getRandomTopGames(authenticateRes.access_token, 72);
+	cachedResponse.set(randomGames);
+	unsubscribe();
 	return { randomGames };
 };
