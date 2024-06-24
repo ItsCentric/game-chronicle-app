66bed4e62777c1cd52eec6a2351c5b8c010ec194
isaiah <isaiahmstockton@gmail.com>
2024-06-24 01:59:18 -0400

    feat: remove autocomplete from input and textarea fields

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
-	import type { HTMLInputAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils.js";
-	import type { InputEvents } from "./index.js";
+	import type { HTMLInputAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
+	import type { InputEvents } from './index.js';
 
 	type $$Props = HTMLInputAttributes;
 	type $$Events = InputEvents;
 
-	let className: $$Props["class"] = undefined;
-	export let value: $$Props["value"] = undefined;
+	let className: $$Props['class'] = undefined;
+	export let value: $$Props['value'] = undefined;
 	export { className as class };
 </script>
 
 <input
 	class={cn(
-		"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+		'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
 		className
 	)}
+	autocomplete="off"
 	bind:value
 	on:blur
 	on:change
