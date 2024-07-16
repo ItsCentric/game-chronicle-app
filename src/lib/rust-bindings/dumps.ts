6a24c41520b178498382a6b376032a2c15958973
Isaiah <isaiahmstockton@gmail.com>
2024-07-15 23:41:11 -0400

    refactor: remove log statements

--- src/lib/rust-bindings/dumps.ts
+++ src/lib/rust-bindings/dumps.ts
@@ -17,33 +17,23 @@ const dumpInfoSchema = z.object({
 type DumpInfo = z.infer<typeof dumpInfoSchema>;
 
 export async function getLocalDumpVersions() {
-	console.log('getLocalDumpVersions');
 	const dumpVersions = await invoke('get_local_dump_versions');
-	console.log('dumpVersions', dumpVersions);
 	return dumpVersionsSchema.parse(dumpVersions);
 }
 
 export async function saveLocalDumpVersions(dumpVersions: DumpVersions) {
-	console.log('saveLocalDumpVersions');
 	await invoke('save_local_dump_versions', { dumpVersions });
-	console.log('saveLocalDumpVersions done');
 }
 
 export async function getAllDumpInfo() {
-	console.log('getAllDumpInfo');
 	const dumpsInfo = await invoke('get_all_dump_info');
-	console.log('dumpsInfo', dumpsInfo);
 	return z.array(dumpInfoSchema).parse(dumpsInfo);
 }
 
 export async function downloadDumps(dumpInfo: DumpInfo[], toDirectory: string) {
-	console.log('downloadDumps');
 	await invoke('download_dumps', { dumpInfo, toDirectory });
-	console.log('downloadDumps done');
 }
 
 export async function importDumps(fromDirectory: string) {
-	console.log('importDumps');
 	await invoke('import_dumps', { fromDirectory });
-	console.log('importDumps done');
 }
