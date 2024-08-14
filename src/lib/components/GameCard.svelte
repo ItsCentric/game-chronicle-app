5964e650b82747575a7e1df8cfc292c4d207c115
Isaiah <isaiahmstockton@gmail.com>
2024-08-14 18:28:17 -0400

    feat: redesign dashboard

--- src/lib/components/GameCard.svelte
+++ src/lib/components/GameCard.svelte
@@ -1,40 +1,48 @@
 <script lang="ts">
-	import { Gamepad2 } from 'lucide-svelte';
-	import type { GameInfo } from '$lib/rust-bindings/igdb';
+	import { Gamepad2, StarIcon } from 'lucide-svelte';
+	import { cn } from '$lib/utils';
 
-	export let data: GameInfo;
+	export let title: string;
+	export let cover: string | undefined | null;
+	export let rating: number | undefined | null;
 </script>
 
 <div
-	class="w-full relative aspect-[3/4] group"
+	{...$$restProps}
+	class={cn('px-4 py-2 border relative rounded-lg group flex gap-4', $$restProps['class'])}
 	data-testid="game-card"
-	on:click
-	on:keydown
-	role="button"
-	tabindex="0"
 >
-	{#if data.cover_image_id}
-		<img
-			src={'https://images.igdb.com/igdb/image/upload/t_cover_big/' + data.cover_image_id + '.jpg'}
-			alt="cover"
-			class="h-full rounded-3xl"
-		/>
-	{:else}
-		<span
-			class="h-full from-primary rounded-3xl to-secondary bg-gradient-to-br flex justify-center items-center"
-		>
-			<p class="select-none text-muted">
-				<Gamepad2 size={64} />
-			</p>
-		</span>
-	{/if}
-	<slot />
-	<span
-		class="opacity-0 z-10 group-hover:opacity-70 transition-opacity bg-black absolute inset-0 rounded-3xl"
-	/>
-	<p
-		class="absolute font-heading z-20 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity text-center font-semibold"
+	<div class="relative aspect-[3/4] max-w-[10rem] flex-1 h-full group">
+		{#if cover}
+			<img
+				src={'https://images.igdb.com/igdb/image/upload/t_cover_big/' + cover + '.jpg'}
+				alt="cover"
+				class="rounded-lg"
+			/>
+		{:else}
+			<span
+				class="h-full from-primary rounded-3xl to-secondary bg-gradient-to-br flex justify-center items-center"
+			>
+				<p class="select-none text-muted">
+					<Gamepad2 size={64} />
+				</p>
+			</span>
+		{/if}
+	</div>
+	<div class="flex-1">
+		<div class="mb-4">
+			<h3 class="text-lg font-semibold">{title}</h3>
+			<slot name="sub-title" class="text-sm text-muted-foreground" />
+		</div>
+		<div class="text-foreground mb-4 flex gap-1 items-center">
+			<StarIcon />
+			<p>{rating === null || rating == undefined ? 'N/A' : rating.toFixed(1)}</p>
+		</div>
+		<slot name="description" />
+	</div>
+	<div
+		class="opacity-0 -bottom-2 group-hover:opacity-100 duration-200 transition-opacity flex gap-1 -right-2 bg-background z-50 absolute border rounded-md px-2 py-1"
 	>
-		{data.title}
-	</p>
+		<slot name="actions" />
+	</div>
 </div>
